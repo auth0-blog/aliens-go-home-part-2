8961326fc4acb766fe6b096d93645c9858c76366
Bruno Krebs <bruno.krebs@auth0.com>
2018-01-31 17:57:16 -0200

    adding CurrentScore component and Google Font;

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -5,6 +5,7 @@ import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
 import CannonBall from './CannonBall';
+import CurrentScore from './CurrentScore'
 
 const Canvas = (props) => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
@@ -15,11 +16,17 @@ const Canvas = (props) => {
       onMouseMove={props.trackMouse}
       viewBox={viewBox}
     >
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
       <Sky />
       <Ground />
       <CannonPipe rotation={props.angle} />
       <CannonBase />
       <CannonBall position={{x: 0, y: -100}}/>
+      <CurrentScore score={15} />
     </svg>
   );
 };
