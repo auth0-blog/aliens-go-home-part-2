7a6c8292024ca70662497c647675856d94c585e1
Bruno Krebs <bruno.krebs@auth0.com>
2018-02-01 15:01:00 -0200

    adding the Title component;

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -9,6 +9,7 @@ import CurrentScore from './CurrentScore'
 import FlyingObject from './FlyingObject';
 import Heart from './Heart';
 import StartGame from './StartGame';
+import Title from './Title';
 
 const Canvas = (props) => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
@@ -34,6 +35,7 @@ const Canvas = (props) => {
       <FlyingObject position={{x: 150, y: -300}}/>
       <Heart position={{x: -300, y: 35}} />
       <StartGame onClick={() => console.log('Aliens, Go Home!')} />
+      <Title />
     </svg>
   );
 };
